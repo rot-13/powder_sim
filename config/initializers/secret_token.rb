4e1f512e54ed0e152a94d0ba26fef85518420fc8
Shay Davidson <shay.h.davidson@gmail.com>
2012-12-12 16:22:33 +0200

    Init rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AGameOfLife::Application.config.secret_token = '15703efb836179ac495193099b1b17cab187a33fa3483745d145ef6f9652864ed4c183c9272faccb7b56010eb558c8a4cbc8f85707fc7bda3b56c4a348a58b5d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AGameOfLife::Application.config.session_store :cookie_store, key: '_a_game_of_life_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AGameOfLife::Application.config.session_store :active_record_store
